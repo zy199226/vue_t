d9c34cbbd4a0790db456827ab239722d34dadb3c
zy199226 <122203566@qq.com>
2017-07-11 17:13:30 +0800

    vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+	<div id="example">
+		<h1>{{msg}}</h1>
+		<ul>
+			<li v-for="n in 5">{{n}}</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	data () {
+		return {
+			msg: 'Hello World!'
+		}
+	}
+}
+</script>
+
+<style lang="less">
+#example{
+	font-size: 1.5em;
+	color: #abcdef;
+	font-weight: 400;
+	ul{
+		li{
+			color: #999;
+			font-size: 14px;
+		}
+	}
+}
+</style>
