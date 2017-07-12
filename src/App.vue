c140ea4b1a2bf3de4ef528caadf7821c60be03d5
zy199226 <122203566@qq.com>
2017-07-12 14:55:45 +0800

    测试less的引入编译正确

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template lang="html">
 	<div id="example">
-		<h1>{{msg}}</h1>
+		<h1 v-if="seen">{{msg}}</h1>
 		<ul>
 			<li v-for="n in 5">{{n}}</li>
+			<li :title="message">{{msg}}</li>
+			<li v-for="todo in todos">{{todo.text}}</li>
 		</ul>
+		<p>{{abc}}</p>
+		<input v-model="abc">
 	</div>
 </template>
 
@@ -11,9 +15,23 @@
 export default {
 	data () {
 		return {
-			msg: 'Hello World!'
+			msg: 'Hello World!',
+			message: '页面加载于：' + new Date(),
+			seen: true,
+			todos: [
+				{text: '学习javascript'},
+				{text: '学习vue'},
+				{text: '学习整个项目'}
+			],
+			abc: 'abc',
+
 		}
 	}
+	// methods () {
+	// 	click: function () {
+	// 		console.log(123);
+	// 	}
+	// }
 }
 </script>
 
@@ -24,7 +42,7 @@ export default {
 	font-weight: 400;
 	ul{
 		li{
-			color: #999;
+			color: pink;
 			font-size: 14px;
 		}
 	}
