f04f743bfe4220d3dc77a7196a76eeb0f3c9f5a9
zy199226 <122203566@qq.com>
2017-08-06 12:01:38 +0800

    Revert "Create README.md"
    This reverts commit 18dc2347ce00d9a01f5dee259dd6f6eb5888d48b.

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js',
-        publicPath: '/dist/'
+        publicPath: 'dist/'
     },
 
     devtool: 'eval',
