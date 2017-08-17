0f30ede5b4f54374930aac4d468d7dc87a96f7b5
zy199226 <122203566@qq.com>
2017-08-17 17:12:46 +0800

    user

--- src/containers/User.vue
+++ src/containers/User.vue
@@ -1,10 +1,34 @@
 <template lang="html">
-    
+    <Mains>
+        <SideBar slot="sideBar" :user="user" :visibility="true"></SideBar>
+        
+    </Mains>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import Mains from '../components/main/main.vue';
+import Panel from '../components/panel/panel.vue';
+import SideBar from '../components/sideBar/sideBar.vue';
+
 export default {
-}
+    components: {
+        Mains,
+        Panel,
+        SideBar
+    },
+    created() {
+        this.$store.dispatch('axiosUserDetail', this.$route.params.loginname);
+    },
+    watch: {
+        $route(e) {
+            this.$store.dispatch('axiosUserDetail', e.params.loginname);
+        }
+    },
+    computed: mapState({
+        user: state => state.userDetail
+    })
+};
 </script>
 
 <style lang="less">
